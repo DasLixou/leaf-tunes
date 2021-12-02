3eb471f8260054d20175fc0a85f56ed0c26c8365
DasLixou <einlixou@gmail.com>
2021-12-02 16:20:55 +0100

    📏 Changed Sizes

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,12 +2,17 @@ import { SearchIcon } from '@heroicons/react/outline'
 
 const Sidebar = () => {
   return (
-    <div className="sm:w-[192px] lg:w-[240px] top-0 left-0 h-screen m-0 flex flex-col bg-indigo-50 pt-3">
+    <div className="sm:w-[192px] lg:w-[240px] top-0 left-0 h-screen m-0 flex flex-col bg-indigo-50 pt-3 px-2 gap-y-3">
       {/** Search bar */}
-      <div className="w-4/5 h-8 mx-auto items-center bg-gray-50 rounded-md flex">
+      <div className="w-11/12 h-8 mx-auto items-center bg-gray-50 rounded-md flex">
         <SearchIcon className="w-5 ml-2.5 text-gray-700" />
-        <input className="w-9/12 h-full block mr-3 ml-auto bg-transparent focus:outline-none rounded-md" placeholder="Search.." type="text" />
+        <input className="w-9/12 h-full block mr-3 ml-auto bg-transparent text-sm focus:outline-none rounded-md" placeholder="Search.." type="text" />
       </div>
+
+      {/** Section */}
+      <section className="">
+        <h1 className="text-sm">Your Playlists</h1>
+      </section>
     </div>
   );
 };
