00d4c2e9ef62b8c89e413c7a3b680245e4aba51f
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-12-02 21:16:36 +0530

    the header is looking slick!!

--- components/Body.js
+++ components/Body.js
@@ -1,7 +1,47 @@
+import {
+  AdjustmentsIcon,
+  ExclamationCircleIcon,
+  FastForwardIcon,
+  PauseIcon,
+  RewindIcon,
+  VolumeUpIcon
+} from "@heroicons/react/outline";
+
 const Body = () => {
   return (
     <div className="flex-grow h-screen overflow-y-scroll scrollbar-hide">
-      <h1>The Body</h1>
+      <header className="flex justify-between">
+        <div className="flex items-center">
+          <img
+            className="h-[55px] w-[55px] rounded-sm"
+            src="https://avatars.githubusercontent.com/u/70377522?v=4"
+          />
+          <div className="">
+            <h3 className="text-gray-600 text-sm font-bold">Song Name</h3>
+            <h3 className="text-gray-400 text-xs">Blackout - Apr 30, 2019</h3>
+          </div>
+        </div>
+        <div className="pt-2 px-[50px] flex-grow flex flex-col items-center">
+          <div className="flex space-x-4">
+            <RewindIcon className="header_button" />
+            <PauseIcon className="header_button" />
+            <FastForwardIcon className="header_button" />
+          </div>
+          <input type="range" className="w-full" min={0} max={100} />
+        </div>
+        <div className="flex items-center mr-4 space-x-2">
+          <div className="flex items-center">
+            <input type="range" />
+            <VolumeUpIcon className="button_header_2 ml-2" />
+          </div>
+          <div className="flex items-center border-[0.1px] p-2">
+            <ExclamationCircleIcon className="button_header_2 border-r-[0.1px]" />
+            <AdjustmentsIcon className="button_header_2" />
+          </div>
+        </div>
+      </header>
+      <hr className="border-t-[0.1px] border-gray-300" />
+      <main className=""></main>
     </div>
   );
 };
