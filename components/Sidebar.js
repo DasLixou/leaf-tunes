91da89626af5dbe066dfdaab64d7773a6981f2b2
DasLixou <einlixou@gmail.com>
2021-12-02 16:10:41 +0100

    🔎 Added Simple Search Bar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,10 @@
 const Sidebar = () => {
   return (
-    <div className="sm:max-w-[192px] lg:max-w-[240px]">
-      <h1>The Sidebar</h1>
+    <div className="sm:w-[192px] lg:w-[240px] top-0 left-0 h-screen m-0 flex flex-col bg-indigo-50 pt-3">
+      {/** Search bar */}
+      <div className="block w-4/5 h-8 mx-auto items-center bg-gray-50 rounded-md">
+        <input className="w-9/12 h-full block mr-3 ml-auto bg-transparent focus:outline-none rounded-md" placeholder="Search.." type="text" />
+      </div>
     </div>
   );
 };
