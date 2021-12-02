c4a4bd2bc673f028dad31bae176c54beef44f152
Jonak-Adipta-Kalita <jonakadiptakalita@gmail.com>
2021-12-02 21:44:29 +0530

    🖼 Changed the Player TItle Pic

--- components/Body.js
+++ components/Body.js
@@ -13,11 +13,11 @@ const Body = () => {
       <header className="flex justify-between">
         <div className="flex items-center">
           <img
-            className="h-[55px] w-[55px] rounded-sm"
-            src="https://avatars.githubusercontent.com/u/70377522?v=4"
+            className="h-[55px] w-[55px] rounded-lg m-3"
+            src="/player_title_pic.png"
           />
           <div className="">
-            <h3 className="text-gray-600 text-sm font-bold">Song Name</h3>
+            <h3 className="text-gray-600 text-sm font-bold">8. Whiteout</h3>
             <h3 className="text-gray-400 text-xs">Blackout - Apr 30, 2019</h3>
           </div>
         </div>
@@ -34,7 +34,7 @@ const Body = () => {
             <input type="range" />
             <VolumeUpIcon className="button_header_2 ml-2" />
           </div>
-          <div className="flex items-center border-[0.1px] p-2">
+          <div className="flex items-center border-[0.1px] p-2 space-x-[2px]">
             <ExclamationCircleIcon className="button_header_2 border-r-[0.1px]" />
             <AdjustmentsIcon className="button_header_2" />
           </div>
