f0b9087f725d125462d243068ffcbcdd7b5e1cbd
DasLixou <einlixou@gmail.com>
2021-12-05 16:47:22 +0100

    🤍 Got Code Simpler

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,9 +9,31 @@ import {
   DuplicateIcon
 } from "@heroicons/react/solid";
 
+const SearchBar = ({}) => {
+  return (
+    <div className="w-11/12 h-8 mx-auto items-center bg-gray-50 rounded-md flex">
+      <SearchIcon className="w-5 ml-2.5 text-gray-700" />
+      <input
+        className="w-9/12 h-full block mr-3 ml-auto bg-transparent text-sm focus:outline-none rounded-md"
+        placeholder="Search.."
+        type="text"
+      />
+    </div>
+  );
+};
+
+const SidebarColumn = ({ title, children }) => {
+  return (
+    <div className="mt-4">
+      <h1 className="text-sm text-gray-500 font-semibold my-2">{title}</h1>
+      <div>{children}</div>
+    </div>
+  );
+};
+
 const Podcasts = ({ Icon, label }) => {
   return (
-    <div className="flex cursor-pointer hover:bg-gray-400 rounded-lg items-center">
+    <div className="flex cursor-pointer p-1 hover:bg-gray-400 transition-colors duration-150 rounded-lg items-center">
       <Icon className="text-white bg-purple-300 h-7 w-7 p-[2px] rounded-lg" />
       <p className="text-xs ml-4">{label}</p>
     </div>
@@ -20,7 +42,7 @@ const Podcasts = ({ Icon, label }) => {
 
 const Library = ({ Icon, label }) => {
   return (
-    <div className="flex cursor-pointer hover:bg-gray-400 rounded-lg items-center">
+    <div className="flex cursor-pointer p-1 hover:bg-gray-400 transition-colors duration-150 rounded-lg items-center">
       <Icon className="text-white bg-blue-300 h-7 w-7 p-[2px] rounded-lg" />
       <p className="text-xs ml-4">{label}</p>
     </div>
@@ -30,34 +52,19 @@ const Library = ({ Icon, label }) => {
 const Sidebar = () => {
   return (
     <div className="hidden sm:w-[192px] lg:w-[240px] top-0 left-0 h-screen m-0 md:flex flex-col bg-[#d1d6e6] pt-3 px-2 gap-y-3 overflow-x-hidden border-r-[0.1px] border-gray-400">
-      <div className="w-11/12 h-8 mx-auto items-center bg-gray-50 rounded-md flex">
-        <SearchIcon className="w-5 ml-2.5 text-gray-700" />
-        <input
-          className="w-9/12 h-full block mr-3 ml-auto bg-transparent text-sm focus:outline-none rounded-md"
-          placeholder="Search.."
-          type="text"
-        />
-      </div>
+      <SearchBar />
       <div className="space-y-7 ml-2">
-        <div className="mt-4">
-          <h1 className="text-sm text-gray-500 font-semibold my-2">
-            Apple Podcasts
-          </h1>
-          <div className="space-y-2">
-            <Podcasts Icon={CogIcon} label="Listen Now" />
-            <Podcasts Icon={StatusOnlineIcon} label="Browser" />
-            <Podcasts Icon={AdjustmentsIcon} label="Top Charts" />
-          </div>
-        </div>
-        <div>
-          <h1 className="text-sm text-gray-500 font-semibold my-2">Library</h1>
-          <div className="space-y-2">
-            <Library Icon={ClockIcon} label="Recently Added" />
-            <Library Icon={DesktopComputerIcon} label="Shows" />
-            <Library Icon={DuplicateIcon} label="Episodes" />
-            <Library Icon={DownloadIcon} label="Downloaded" />
-          </div>
-        </div>
+        <SidebarColumn title="Apple Podcasts">
+          <Podcasts Icon={CogIcon} label="Listen Now" />
+          <Podcasts Icon={StatusOnlineIcon} label="Browser" />
+          <Podcasts Icon={AdjustmentsIcon} label="Top Charts" />
+        </SidebarColumn>
+        <SidebarColumn title="Library">
+          <Library Icon={ClockIcon} label="Recently Added" />
+          <Library Icon={DesktopComputerIcon} label="Shows" />
+          <Library Icon={DuplicateIcon} label="Episodes" />
+          <Library Icon={DownloadIcon} label="Downloaded" />
+        </SidebarColumn>
       </div>
     </div>
   );
